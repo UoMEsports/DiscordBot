21200af721112634936a26e799ee48dde3346383
spoopydale <christian.moulsdale@gmail.com>
2017-10-20 16:04:53 +0100

    Strike update

--- bot.py
+++ bot.py
@@ -3,13 +3,23 @@ SocietyBot
 Christian Moulsdale and Tom Mewett, 2017
 '''
 
-import discord
 import asyncio
+import aiohttp
+import discord
+import smtplib
 
 from configparser import ConfigParser
+from datetime import datetime, timedelta
 from functools import wraps
 from os import listdir, system
 from sys import argv
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+from overwatch_api.core import AsyncOWAPI
+from overwatch_api.constants import *
 
 # this is a zero width seperator
 zero_seperator = '​'
@@ -37,6 +47,52 @@ role_name = config.get('names', 'role')
 society_name = config.get('names', 'society')
 bot_name = config.get('names', 'bot')
 
+'''
+# read in the members from the csv file
+def read_members():
+    members = dict()
+    with open('{}.csv'.format(bot_name), 'r') as f:
+        f.readline()
+        for line in f:
+            id, data = line.strip().split(',', 1)
+            members[id] = data.split(',')
+    return members
+'''
+
+# email shit
+fromaddr = config.get('email', 'fromaddr')
+password = config.get('email', 'password')
+toaddr = config.get('email', 'toaddr')
+
+# backup the members file
+async def backup_members():
+    while True:
+        msg = MIMEMultipart()
+         
+        msg['From'] = fromaddr
+        msg['To'] = toaddr
+        msg['Subject'] = 'Backup of EsportsBot.csv {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M'))
+         
+        filename = '{}.csv'.format(bot_name)
+        attachment = open(filename, 'rb')
+         
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload((attachment).read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment; filename={}'.format(filename))
+         
+        msg.attach(part)
+
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(fromaddr, password)
+        text = msg.as_string()
+        server.sendmail(fromaddr, toaddr, text)
+        server.quit()
+
+        # wait 1 day
+        await asyncio.sleep(86400)
+
 # the esbot class
 class Esbot(discord.Client):
     # initialise the bot
@@ -53,35 +109,29 @@ class Esbot(discord.Client):
     async def safe_send_message(self, destination, content):
         try:
             return await super().send_message(destination, content)
-        except:
-            pass
+        except Exception as ex:
+            print(ex)
 
     # safely send a file
-    async def safe_send_file(self, destination, fp):
+    async def safe_send_file(self, destination, fp, content=None):
         try:
-            return await super().send_file(destination, fp)
-        except:
-            pass
+            return await super().send_file(destination, fp, content)
+        except Exception as ex:
+            print(ex)
 
     # safely delete a message
     async def safe_delete_message(self, message):
         try:
             await super().delete_message(message)
-        except:
-            pass
+        except Exception as ex:
+            print(ex)
 
     # safely add a reaction
     async def safe_add_reaction(self, message, emoji):
         try:
             await super().add_reaction(message, emoji)
-        except:
-            pass
-
-    # respond to a message and then delete the response after a given lifetime with a default of 30s
-    async def temp_say(self, channel, str_response, lifetime=30):
-        response = await self.safe_send_message(channel, str_response)
-        await asyncio.sleep(lifetime)
-        await self.safe_delete_message(response)
+        except Exception as ex:
+            print(ex)
         
     # output to terminal if the bot successfully logs in
     async def on_ready(self):
@@ -100,10 +150,16 @@ class Esbot(discord.Client):
 
         # read server variable ids from the config file and intialise them as global variables
         self.server = discord.utils.get(self.servers, id=config.get('general', 'server_id'))
+        
         self.member_role = discord.utils.get(self.server.roles, id=config.get('roles', 'member_id'))
         self.guest_role = discord.utils.get(self.server.roles, id=config.get('roles', 'guest_id'))
         self.committee_role = discord.utils.get(self.server.roles, id=config.get('roles', 'committee_id'))
+        self.first_strike_role = discord.utils.get(self.server.roles, id=config.get('roles', 'first_strike_id'))
+        self.second_strike_role = discord.utils.get(self.server.roles, id=config.get('roles', 'second_strike_id'))
+        self.third_strike_role = discord.utils.get(self.server.roles, id=config.get('roles', 'third_strike_id'))
+        
         self.esbot_channel = discord.utils.get(self.server.channels, id=config.get('channels', 'esbot_id'))
+        self.moderation_channel = discord.utils.get(self.server.channels, id=config.get('channels', 'moderation_id'))
 
         # change the game to 'type !help for the list of commands'
         await self.change_presence(game=discord.Game(type=0, name='type {}help for the list of commands'.format(self.command_prefix)))
@@ -115,6 +171,39 @@ class Esbot(discord.Client):
         for member in self.server.members:
             if self.member_role not in member.roles and self.guest_role not in member.roles and member != self.user:
                 await self.send_terms(member)
+
+        # initialise the members dictionary
+        self.members = dict()
+        with open('{}.csv'.format(bot_name), 'r') as f:
+            f.readline()
+            for line in f:
+                id, data = line.strip().split(',', 1)
+                self.members[id] = data.split(',')
+
+        # process the unbans
+        asyncio.ensure_future(self.process_unbans())
+
+        # backup the members file
+        asyncio.ensure_future(backup_members())
+
+        '''
+        members = dict()
+        for member in self.server.members:
+            if self.member_role in member.roles:
+                members[member.id] = [member.name, 'member', '0', '', '', '', '', '']
+            elif self.guest_role in member.roles:
+                members[member.id] = [member.name, 'guest', '0', '', '', '', '', '']
+            else:
+                members[member.id] = [member.name, '', '0', '', '', '', '', '']
+        self.write_members()
+        '''
+
+    # write the members to the csv file
+    def write_members(self):
+        with open('{}.csv'.format(bot_name), 'w') as f:
+            f.write('User ID,Username,Role,Number of strikes,Reason 1,Reason 2,Reason 3,Reason 4,Unban date\n')
+            for id in self.members:
+                f.write('{},{}\n'.format(id, ','.join(self.members[id])))
 
     # esborts command wrapper
     def command(usage='', committee_only=False):
@@ -156,17 +245,26 @@ class Esbot(discord.Client):
             else:
                 if message_content.startswith(self.command_prefix):
                     await self.process_commands(message, message_content)
+
+    # when a member updates their profile:
+    async def on_member_update(self, before, after):
+        if before.name != after.name:
+            self.members[after.id][0] = after.name
+            self.write_members()
         
     # check if message is a PM - terms and conditions
     async def accept_terms(self, member, message_content_lower):
         if message_content_lower.startswith('yes'):
             # user is a member
             await self.safe_send_message(member, 'Member role has been added')
-            return await self.add_roles(member, self.member_role)
+            self.members[member.id][2] = 'member'
+            await self.add_roles(member, self.member_role)
         elif message_content_lower.startswith('no'):
             # user is a guest
             await self.safe_send_message(member, 'Guest role has been added')
-            return await self.add_roles(member, self.guest_role)
+            self.members[member.id][2] = 'guest'
+            await self.add_roles(member, self.guest_role)
+        self.write_members()
         
     # check for a command
     async def process_commands(self, message, message_content):
@@ -179,6 +277,7 @@ class Esbot(discord.Client):
             if command in self.commands:
                 kwargs = dict()
                 kwargs['member'] = member
+                kwargs['message'] = message
                 
                 cmd = getattr(self, command, None)
                 await cmd(*args, **kwargs)
@@ -188,23 +287,48 @@ class Esbot(discord.Client):
             await self.safe_delete_message(message)
             await self.safe_send_message(self.esbot_channel, '{} use commands here.'.format(member.mention))
 
-    '''
-    # process the meme responses
-    async def meme_response(self, message, message_content_lower):
-        if 'merci' in message_content_lower:
-            await self.safe_add_reaction(message, self.no_merci_emoji)
-        if 'scrub' in message_content_lower:
-            await self.safe_add_reaction(message, self.team_scrub_emoji)
-    '''
-
     # send the terms and conditions prompts to a member
     async def send_terms(self, member):
         await self.safe_send_message(member, 'Are you a member of the {}? (`yes` or `no`) You can be in this server without being a member as a guest.'.format(society_name))
 
     # when a member joins, send them a PM asking if they accept the terms and conditions
     async def on_member_join(self, member):
+        id = member.id
         await self.safe_send_message(member, 'Welcome to the {} discord server!'.format(society_name))
-        await self.send_terms(member)
+        if id in self.members:
+            roles = []
+            role = self.members[id][1]
+            if role == 'member':
+                roles.append(self.member_role)
+            elif role == 'guest':
+                roles.append(self.guest_role)
+            else:
+                await self.send_terms(member)
+            strikes = self.members[id][2]
+            if strikes == '1':
+                roles.append(self.first_strike_role)
+            elif strikes == '2':
+                roles.append(self.second_strike_role)
+            elif strikes == '3':
+                roles.append(self.third_strike_role)
+            await self.add_roles(member, *roles)
+        else:
+            self.members[id] = [member.name, '', '0', '', '', '', '', '']
+            await self.send_terms(member)
+        self.write_members()
+
+    # process the unbans
+    async def process_unbans(self):
+        while True:
+            for id in self.members:
+                if self.members[id][7] not in ['', 'never']:
+                    date = datetime.strptime(self.members[id][7], '%Y-%m-%d %H:%M:%S')
+                    if datetime.now() > date:
+                        self.members[id][7] = ''
+                        await self.unban(self.server, discord.User(id=id))
+                        await self.send_message(self.moderation_channel, '{} has been unbanned'.format(self.members[0]))
+            self.write_members()
+            await asyncio.sleep(3600)
 
     # BOT COMMANDS
  
@@ -388,6 +512,69 @@ class Esbot(discord.Client):
             await self.safe_send_message(self.esbot_channel, 'Changed presence to `{}`.'.format(presence))
         else:
             raise UsageException
-        
+
+    # get bnet stats for a user
+    @command(usage='battletag')
+    async def stats(self, *args, **kwargs):
+        if len(args) != 0:
+            client = AsyncOWAPI()
+            try:
+                data = await client.get_profile(args[0])
+            except asyncio.TimeoutError:
+                return await self.safe_send_message(self.esbot_channel, 'Timed out, :slight_frown:')
+            if data != {}:
+                try:
+                    SR = data['eu']['stats']['competitive']['overall_stats']['comprank']
+                    tier = data['eu']['stats']['competitive']['overall_stats']['tier']
+                    if SR == None:
+                        SR = ''
+                    await self.safe_send_message(self.esbot_channel, '`{}: {}({})`'.format(args[0], tier.title(), SR))
+                except KeyError:
+                    return await self.safe_send_message(self.esbot_channel, '{} has never played on EU, :slight_frown:'.format(args[0]))
+            else:
+                await self.safe_send_message(self.esbot_channel, 'Unable to find profile `{}`.'.format(args[0]))
+        else:
+            raise UsageException
+
+    # give a user a strike
+    @command(usage='user reason', committee_only=True)
+    async def strike(self, *args, **kwargs):
+        message = kwargs['message']
+        if message.mentions != None and len(args) == 2:
+            reason = args[1]
+            for member in message.mentions:
+                id = member.id
+                if self.members[id][2] == '0':
+                    self.members[id][2] = '1'
+                    self.members[id][3] = reason
+                    await self.safe_send_message(member, 'You have been given a first strike for `{}`. Please follow the rules.'.format(reason))
+                    await self.safe_send_message(self.moderation_channel, '{} has been given a first strike.'.format(member.name))
+                    await self.add_roles(member, self.first_strike_role)
+                elif self.members[id][2] == '1':
+                    self.members[id][2] = '2'
+                    self.members[id][4] = reason
+                    await self.safe_send_message(member, 'You have been given a second strike for `{}`. Please follow the rules.'.format(reason))
+                    await self.safe_send_message(self.moderation_channel, '{} has been given a second strike.'.format(member.name))
+                    await self.remove_roles(member, self.first_strike_role)
+                    await self.add_roles(member, self.second_strike_role)
+                elif self.members[id][2] == '2':
+                    self.members[id][2] = '3'
+                    self.members[id][5] = reason
+                    unban_date = (datetime.now() + timedelta(days=7)).strftime('%Y-%m-%d %H:%M:%S')
+                    self.members[id][7] = unban_date
+                    await self.safe_send_message(member, 'You have been given a 7 day ban for `{}`. Your ban will expire at `{}`'.format(reason, unban_date))
+                    await self.safe_send_message(self.moderation_channel, '{} has been given a 7-day ban.'.format(member.name))
+                    await self.ban(member)
+                else:
+                    self.members[id][2] = '4'
+                    self.members[id][6] = reason
+                    self.members[id][7] = 'never'
+                    await self.safe_send_message(member, 'You have been permanently banned for `{}`.'.format(reason))
+                    await self.safe_send_message(self.moderation_channel, '{} has been permanently banned.'.format(member.name))
+                    await self.ban(member)
+            self.write_members()
+        else:
+            raise UsageException
+
 # start the bot
 Esbot()
